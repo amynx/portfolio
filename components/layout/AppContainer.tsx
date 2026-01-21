3e5950bd3888d6e92095d586b7020413ab5a336d
amynx <andresyepezmn@gmail.com>
2026-01-21 16:56:41 -0500

    refactor(layout): move padding from AppContainer to SectionInner

--- components/layout/AppContainer.tsx
+++ components/layout/AppContainer.tsx
@@ -16,10 +16,6 @@ export default function AppContainer({ children }: AppContainerProps) {
         max-w-[1600px]
         grid
         grid-rows-[auto_auto]
-        px-4
-        sm:px-6
-        lg:px-8
-        
       ">
                 {children}
             </div>
